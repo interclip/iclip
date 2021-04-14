53721d41a5508264ac88c7cc7a45429626728183
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-14 16:31:40 +0000

    Delete unused packages

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk");
-const clear = require('clear');
 const figlet = require('figlet');
 const validator = require('validator');
 const qrcode = require('qrcode-terminal');
